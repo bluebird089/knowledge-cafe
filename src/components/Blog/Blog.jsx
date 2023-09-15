4cc9b54d70ae0a8b4d5bb299d79c648ea6b09c5e
bluebird089 <bluebird089@proton.me>
2023-09-16 00:23:28 +0600

    added the blogs

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    const { title, cover, author_img, author, posted_date, reading_time, hashtags } = blog;
     return (
-        <div>
-
+        <div className='space-y-3 py-3 border-b-[1px]'>
+            <img className='w-full rounded-2xl' src={cover} alt={`Cover of title: ${title}`} />
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center gap-2'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                        <h5 className='font-bold text-lg'>{author}</h5>
+                        <p className='text-sm text-gray-500'>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='text-gray-500'>{reading_time} min read</p>
+                </div>
+            </div>
+            <h2 className='text-3xl font-bold'>{title}</h2>
+            <div>
+                {
+                    hashtags.map((hashtag, idx) => <span className='text-gray-500 text-sm mr-3' key={idx}><a href="">#{hashtag}</a></span>)
+                }
+            </div>
+            <p className='text-[#6047EC]'><a className='hover:underline' href="">Mark as read</a></p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,18 +1,22 @@
-import { data } from "autoprefixer";
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
         fetch('blog.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data));
+            .then(res => res.json())
+            .then(data => setBlogs(data));
     }, [])
 
     return (
-        <div className="md:w-2/3">
-            <h4>Blogs: {blogs.length}</h4>
+        <div className="md:w-2/3 space-y-3">
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}></Blog>)
+            }
         </div>
     );
 };
