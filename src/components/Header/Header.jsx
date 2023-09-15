6d4463a1df504fce0abd9ccf73460705494f7ec6
bluebird089 <bluebird089@proton.me>
2023-09-15 21:05:36 +0600

    added header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import Profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between items-center p-2 container mx-auto border-b-[1px]'>
             <h1 className="text-4xl">Knowledge Cafe</h1>
+            <img src={Profile} alt="" />
         </div>
     );
 };
